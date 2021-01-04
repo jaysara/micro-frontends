bc53f60767bdd2fb0bcef24cfb80eac4d6a8250c
jaysara <saraiya.jay@gmail.com>
2021-01-03 22:48:18 -0600

    triggering build 3

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
     filename: '[name].[contenthash].js',
     publicPath: '/marketing/latest/',
   },
+  
   plugins: [
     new ModuleFederationPlugin({
       name: 'marketing',
