b46b1d59aff379e2ac487922c4f9f79a1df8bd1e
jaysara <saraiya.jay@gmail.com>
2021-01-09 18:27:23 -0600

    add auth package

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import {  BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
 import MarketingApp from './components/MarketingApp';
-import Header from './components/Header'
-import { StylesProvider , createGenerateClassname} from '@material-ui/core/styles';
+import Header from './components/Header';
 
-
-const generateClassname = createGenerateClassname({
-  productionPrefix :'co',
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
 });
-
 
 export default () => {
   return (
     <BrowserRouter>
-      <StylesProvider generateClassname={generateClassname}>
+      <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
           <MarketingApp />
